89ffb0a9236dc77d24d9c4110383287ec28112a6
kitchWWW <brian.e2014@gmail.com>
2020-05-30 12:26:06 -0700

    now with two more insturments almost

--- sketch.js
+++ sketch.js
@@ -43,6 +43,10 @@
 var canPlay = false;
 var shouldPlay = false;
 var justPlayed = false;
+var CHOSEN_INSTURMENT = '';
+var distortion;
+var flanger;
+var quadrafuzz;
 
 let video;
 let poseNet;
@@ -60,35 +64,118 @@ function setup() {
   poseNet.on('pose', gotPoses);
 
 }
-var eDown = new Pizzicato.Sound('eDown.wav', function() {});
-var eUp = new Pizzicato.Sound('eUp.wav', function() {});
-var aDown = new Pizzicato.Sound('aDown.wav', function() {});
-var aUp = new Pizzicato.Sound('aUp.wav', function() {});
-var bDown = new Pizzicato.Sound('bDown.wav', function() {});
-var bUp = new Pizzicato.Sound('bUp.wav', function() {});
+var flying_1d = new Pizzicato.Sound('res_audio/flying/eDown.wav', function() {});
+var flying_1u = new Pizzicato.Sound('res_audio/flying/eUp.wav', function() {});
+var flying_2d = new Pizzicato.Sound('res_audio/flying/aDown.wav', function() {});
+var flying_2u = new Pizzicato.Sound('res_audio/flying/aUp.wav', function() {});
+var flying_3d = new Pizzicato.Sound('res_audio/flying/bDown.wav', function() {});
+var flying_3u = new Pizzicato.Sound('res_audio/flying/bUp.wav', function() {});
 
-var group = new Pizzicato.Group([eDown, eUp, aDown, aUp, bDown, bUp]);
+var acoustic_1d = new Pizzicato.Sound('res_audio/acoustic/gDown.wav', function() {});
+var acoustic_1u = new Pizzicato.Sound('res_audio/acoustic/gUp.wav', function() {});
+var acoustic_2d = new Pizzicato.Sound('res_audio/acoustic/cDown.wav', function() {});
+var acoustic_2u = new Pizzicato.Sound('res_audio/acoustic/cUp.wav', function() {});
+var acoustic_3d = new Pizzicato.Sound('res_audio/acoustic/dDown.wav', function() {});
+var acoustic_3u = new Pizzicato.Sound('res_audio/acoustic/dUp.wav', function() {});
 
-var distortion = new Pizzicato.Effects.Distortion({
-    gain: 0.4
-});
-group.addEffect(distortion);
+var metal_1d = new Pizzicato.Sound('res_audio/flying/eDown.wav', function() {});
+var metal_1u = new Pizzicato.Sound('res_audio/flying/eUp.wav', function() {});
+var metal_2d = new Pizzicato.Sound('res_audio/flying/aDown.wav', function() {});
+var metal_2u = new Pizzicato.Sound('res_audio/flying/aUp.wav', function() {});
+var metal_3d = new Pizzicato.Sound('res_audio/flying/bDown.wav', function() {});
+var metal_3u = new Pizzicato.Sound('res_audio/flying/bUp.wav', function() {});
 
 
-function stopAll(){
-  try{eDown.stop();}catch{}
-  try{eUp.stop();}catch{}
-  try{aDown.stop();}catch{}
-  try{aUp.stop();}catch{}
-  try{bDown.stop();}catch{}
-  try{bUp.stop();}catch{}
+function stopAll() {
+  try {
+    chord_1d.stop();
+  } catch {}
+  try {
+    chord_1u.stop();
+  } catch {}
+  try {
+    chord_2d.stop();
+  } catch {}
+  try {
+    chord_2u.stop();
+  } catch {}
+  try {
+    chord_3d.stop();
+  } catch {}
+  try {
+    chord_3u.stop();
+  } catch {}
 }
 
 
 function preload() {
-  img = loadImage('guitar.png');
-
+  img_flying = loadImage('res_img/flying.png');
+  img_acoustic = loadImage('res_img/acoustic.png');
+  img_metal = loadImage('res_img/metal.png');
+  img = img_flying;
 }
+
+
+function chooseInsturment(choice) {
+  CHOSEN_INSTURMENT = choice;
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+  console.log(choice);
+  if (choice == 'flying') {
+    img = img_flying;
+    chord_1d = flying_1d;
+    chord_1u = flying_1u;
+    chord_2d = flying_2d;
+    chord_2u = flying_2u;
+    chord_3d = flying_3d;
+    chord_3u = flying_3u;
+
+    var group = new Pizzicato.Group([chord_1d, chord_1u, chord_2d, chord_2u, chord_3d, chord_3u]);
+    distortion = new Pizzicato.Effects.Distortion({
+      gain: 0.4
+    });
+    group.addEffect(distortion);
+  } else if (choice == 'metal') {
+    img = img_metal
+    chord_1d = metal_1d;
+    chord_1u = metal_1u;
+    chord_2d = metal_2d;
+    chord_2u = metal_2u;
+    chord_3d = metal_3d;
+    chord_3u = metal_3u;
+    var group = new Pizzicato.Group([chord_1d, chord_1u, chord_2d, chord_2u, chord_3d, chord_3u]);
+    quadrafuzz = new Pizzicato.Effects.Quadrafuzz({
+      lowGain: 0.6,
+      midLowGain: 0.8,
+      midHighGain: 0.7,
+      highGain: 0.6,
+      mix: 1.0
+    });
+    group.addEffect(quadrafuzz);
+
+  } else { // acoustic
+    img = img_acoustic
+    chord_1d = acoustic_1d;
+    chord_1u = acoustic_1u;
+    chord_2d = acoustic_2d;
+    chord_2u = acoustic_2u;
+    chord_3d = acoustic_3d;
+    chord_3u = acoustic_3u;
+    var group = new Pizzicato.Group([chord_1d, chord_1u, chord_2d, chord_2u, chord_3d, chord_3u]);
+
+    flanger = new Pizzicato.Effects.Flanger({
+      time: 0.45,
+      speed: 0.2,
+      depth: 0.1,
+      feedback: 0.1,
+      mix: 0.5
+    });
+    group.addEffect(flanger);
+
+  }
+  canPlay = true;
+}
+
 
 function gotPoses(poses) {
   //console.log(poses);
@@ -106,6 +193,8 @@ let previouslyAbove = null;
 guitar_len = 700;
 rhx = 500;
 rhy = 500;
+lwy = 400;
+lwx = 400;
 
 function draw() {
   background(255);
@@ -122,17 +211,22 @@ function draw() {
 
     }
 
-    lwx = pose.leftWrist.x;
-    lwy = pose.leftWrist.y;
+    lwx_raw = pose.leftWrist.x;
+    lwy_raw = pose.leftWrist.y;
     rwx = pose.rightWrist.x;
     rwy = pose.rightWrist.y;
-    rhx = ((pose.rightHip.x-guitar_len*.05) * .1) + (rhx * .9);
-    rhy = ((pose.rightHip.y-guitar_len*.05) * .1) + (rhy * .9);
+    rhx = ((pose.rightHip.x - guitar_len * .05) * .1) + (rhx * .9);
+    rhy = ((pose.rightHip.y - guitar_len * .05) * .1) + (rhy * .9);
     nx = pose.nose.x;
     ny = pose.nose.y;
+    lwx = (lwx_raw * .1) + (lwx * .9);
+    lwy = (lwy_raw * .1) + (lwy * .9);
+
+    // we want to change distances quickly, but guitar verticality should be smoothed.
+    var dist_lhFromHip = Math.sqrt(Math.pow((lwx_raw - rhx), 2) + Math.pow((lwy_raw - rhy), 2));
 
 
-    var dist_lhFromHip = Math.sqrt(Math.pow((lwx - rhx), 2) + Math.pow((lwy - rhy), 2));
+
     var dist_rwFromHip = Math.sqrt(Math.pow((rwx - rhx), 2) + Math.pow((rwy - rhy), 2));
     var dist_nFromHip = Math.sqrt(Math.pow((nx - rhx), 2) + Math.pow((ny - rhy), 2));
 
@@ -143,9 +237,23 @@ function draw() {
 
     var guitarAngleDeg = Math.atan2(lwy - rhy, lwx - rhx) * 180 / Math.PI;
 
-    var gainUpdate = Math.abs(guitarAngleDeg+15) / 90.0;
-    if(gainUpdate>1){gainUpdate = 1-gainUpdate};
-    distortion.gain = gainUpdate * gainUpdate;
+    var gainUpdate = Math.abs(guitarAngleDeg + 15) / 90.0;
+    if (gainUpdate > 1) {
+      gainUpdate = 1 - gainUpdate
+    };
+    if (canPlay) {
+      if (CHOSEN_INSTURMENT == 'flying') {
+        distortion.gain = gainUpdate * gainUpdate;
+      } else if (CHOSEN_INSTURMENT == 'acoustic') {
+        flanger.mix = gainUpdate * gainUpdate;
+      }else{//else metal
+        quadrafuzz.lowGain = gainUpdate * gainUpdate;
+        quadrafuzz.midLowGain = gainUpdate * gainUpdate;
+        quadrafuzz.midHighGain = gainUpdate * gainUpdate;
+        quadrafuzz.highGain = gainUpdate * gainUpdate;
+      }
+
+    }
 
     var strumAngleDeg = Math.atan2(rwy - rhy, rwx - rhx) * 180 / Math.PI;
 
@@ -177,17 +285,17 @@ function draw() {
           console.log(shouldPlay);
           console.log(canPlay);
           console.log(justPlayed);
-          
+
           if (shouldPlay && canPlay && (!justPlayed)) {
             if (dist_lhFromHip > guitar_len * mark1) {
               stopAll();
-              eUp.play(0);
+              chord_1u.play(0);
             } else if (dist_lhFromHip > guitar_len * mark2) {
               stopAll();
-              aUp.play(0);
+              chord_2u.play(0);
             } else {
               stopAll();
-              bUp.play(0);
+              chord_3u.play(0);
             }
             justPlayed = true;
             setTimeout(function() {
@@ -199,13 +307,13 @@ function draw() {
           if (shouldPlay && canPlay && (!justPlayed)) {
             if (dist_lhFromHip > guitar_len * mark1) {
               stopAll();
-              eDown.play(0);
+              chord_1d.play(0);
             } else if (dist_lhFromHip > guitar_len * mark2) {
               stopAll();
-              aDown.play(0);
+              chord_2d.play(0);
             } else {
               stopAll();
-              bDown.play(0);
+              chord_3d.play(0);
             }
             justPlayed = true;
             setTimeout(function() {
